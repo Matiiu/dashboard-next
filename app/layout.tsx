39b8779172f5ac0f2d6816c396aa758f8b298a63
Matiiu <mateotaveraramirez@gmail.com>
2024-12-03 09:59:48 -0500

    feat: Add initial dashboard layout and pages with Prettier configuration

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,15 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
-  return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
-  );
+import clsx from 'clsx';
+import '@/app/ui/global.css';
+import { inter } from '@/app/ui/fonts';
+
+type RootLayoutProps = {
+	children: React.ReactNode;
+};
+
+export default function RootLayout({ children }: RootLayoutProps) {
+	return (
+		<html lang="en">
+			<body className={clsx(inter.className, 'antialiased')}>{children}</body>
+		</html>
+	);
 }
